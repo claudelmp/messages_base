3aa291a9aebfb41b5bf55cc13601eea2e8c5255c
claudelmp <claude.fichet@ac-poitiers.fr>
2020-06-18 17:27:13 +0200

    deuxième commit

--- accueil.php
+++ accueil.php
@@ -2,6 +2,7 @@
     <li><a href='index.php?action=saisiemess'> Saisir un message</a>
     <li><a href='index.php?action=listemess'> Lister les messages</a>
     <li><a href='index.php?action=supprmess'> Supprimer les messages</a>
+    <hr>
 
 </ul>
 
